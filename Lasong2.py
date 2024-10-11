58e875bab81669bb3edd0622e263d8b0d85c2e3d
Віталій <git@github.com:Vandalij/My-begin.git>
2024-10-11 20:24:24 +0200

    changes

---
+++ Lasong2.py
@@ -0,0 +1,61 @@
+#int -> ціле число 12
+
+#number = 10+2
+#print(number)
+#number = 20.5
+#print(number)
+#print(type(number))
+#number = "Vasia"
+#print(number)
+#print(type(number))
+#number = True
+#print(number)
+#print(type(number))
+
+#a1 = 10
+#a2 = 20
+#first_number = 10
+
+
+############
+
+#number
+#print(number := 10) #моржовий оператор
+#print(number := 10+1)
+
+
+#########
+# оператори математичні і логічні
+#   унарні - меже бути тільки одтин операнд (наприклад -5) тут мінус унарний
+
+
+# print(2 + 3)
+# print(2 - 3)
+# print(2 * 3)
+# print(2 / 3)
+# print(2 ** 3)
+# print(2 // 3)
+# print( 2 % 3)
+
+
+##########
+
+number = 123
+n1 = number // 100
+#v1
+n2 = number // 10 % 10
+#v2
+#n2 = number % 100 //10
+n3 = number % 10
+
+print(n1)
+print(n2)
+print(n3)
+#
+result  = n1 + n2 + n3
+#
+print("Result: ", result)
+
+
+
+
