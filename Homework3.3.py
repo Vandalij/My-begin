2ee97cdde512411b7957d7c5a03aebbc840527f2
Віталій <git@github.com:Vandalij/My-begin.git>
2024-10-28 19:39:51 +0100

    changes

---
+++ Homework3.3.py
@@ -0,0 +1,12 @@
+def split_list(lst):
+
+    mid = (len(lst) + 1) // 2
+    first_half = lst[:mid]
+    second_half = lst[mid:]
+    return [first_half, second_half]
+
+print(split_list([1, 2, 3, 4, 5, 6]))
+print(split_list([1, 2, 3]))
+print(split_list([1, 2, 3, 4, 5]))
+print(split_list([1]))
+print(split_list([]))
