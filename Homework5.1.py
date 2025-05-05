bc0555bf056eb488d8a702f76617407f9e11ede0
Віталій <git@github.com:Vandalij/My-begin.git>
2025-05-05 16:44:46 +0200

    chainge homework

--- Homework5.1.py
+++ Homework5.1.py
@@ -8,31 +8,16 @@ def is_valid_variable_name(name):
         return False
     if any(char.isupper() for char in name):
         return False
-    if any(char in (set(string.punctuation) - {"_"}) or char.isspace() for char in name):
+    if any((char in string.punctuation and char != "_") or char.isspace() for char in name):
         return False
-    if "__" in name:
+    if name == "_":
+        return True
+    if name.count("_") == len(name):
         return False
     if name in keyword.kwlist:
         return False
     return True
-examples = [
-    "_",
-    "__",
-    "___",
-    "x",
-    "get_value",
-    "get value",
-    "get!value",
-    "some_super_puper_value",
-    "Get_value",
-    "get_Value",
-    "getValue",
-    "3m",
-    "m3",
-    "assert",
-    "assert_exception"
-]
 
-for name in examples:
-    result = is_valid_variable_name(name)
-    print(f"{name} => {result}")
+name = input("Enter a variable name: ")
+result = is_valid_variable_name(name)
+print(result)
