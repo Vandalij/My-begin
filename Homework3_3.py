dd6562864f5df4ced6206c15272bde0feeec2b63
Віталій <git@github.com:Vandalij/My-begin.git>
2025-05-07 09:03:02 +0200

    chainge name and code

--- Homework3_3.py
+++ Homework3_3.py
@@ -1,8 +1,8 @@
 def split_list(lst):
 
-    mid = (len(lst) + 1) // 2
-    first_half = lst[:mid]
-    second_half = lst[mid:]
+    midl = (len(lst) + 1) // 2
+    first_half = lst[:midl]
+    second_half = lst[midl:]
     return [first_half, second_half]
 
 print(split_list([1, 2, 3, 4, 5, 6]))
